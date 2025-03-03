65df89b72dcd5625596b1e6a22abcfcbd384e4f5
MeghanaMPatil <132448612+MeghanaMPatil@users.noreply.github.com>
2025-03-03 11:42:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 20000
+    };
+    return member;
+}
